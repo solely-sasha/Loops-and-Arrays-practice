1845a4fc93e94bed04344605c5161f1e252b0b6a
sasha sims <solely.sashasims@gmail.com>
2022-11-03 21:10:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// 1. Loop through the following array and count how many "computers" there are. Log the final count:
+
+
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+
+// 2. Loop through the following array and log to the console "old enough" if they are 18 or older, and "not old enough" if they aren't 18.
+
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+
+// bonus challenge #1 Log to the console a personalized message like:
+
+
+// bonus challenge #3 Check to see if the movie goer is a male or female for an even more personalized message.
